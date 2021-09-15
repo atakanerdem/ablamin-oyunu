64225840277d4c6e43365f16ba5a99d0f746977f
atakanerdem <atk_erd@hotmail.com>
2021-09-16 00:43:08 +0300

    proje tamamlandı

--- src/main/resources/ablaminoyunu.js
+++ src/main/resources/ablaminoyunu.js
@@ -1,35 +1,77 @@
+var score = 0;
+var clickedButtons = [];
 function createMatrix() {
-	return Array(10).fill(0).map(()=>Array(10).fill(0))
+    return Array(10)
+        .fill(0)
+        .map(() => Array(10).fill(0));
 }
 
-	var matrix = createMatrix();	
+var matrix = createMatrix();
 
-function createTable(){
-	let body = document.getElementById("body");
-	for(var i=0;i<matrix.length;i++){
-		let div = document.createElement("div");
-		for(var j=0;j<matrix.length;j++){
-			let button = document.createElement("button");
-			button.textContent = matrix[i][j];
-			button.value = i.toString() + j.toString();
-			button.addEventListener("click", () => incrementValue(button));
-			div.appendChild(button);
-		}
-		body.appendChild(div);
-	}
+function createTable() {
+    let body = document.getElementById("body");
+    for (var i = 0; i < matrix.length; i++) {
+        let div = document.createElement("div");
+        for (var j = 0; j < matrix.length; j++) {
+            let button = document.createElement("button");
+            button.className = "buttons";
+            button.textContent = matrix[i][j];
+            button.value = i.toString() + j.toString();
+            button.disabled = false;
+            button.addEventListener("click", () => incrementValue(button));
+            button.addEventListener("click", () => disableButtons(button));
+
+            div.appendChild(button);
+        }
+        body.appendChild(div);
+    }
 }
 
-function incrementValue(button){
-	let x = button.value[0];
-	let y = button.value[1];
+function incrementValue(button) {
+    if (score == 0 || !button.disabled) {
+        score++;
+        button.textContent = score;
+        console.log(button.value);
+		clickedButtons.push(button.value);
+    }
 
-	matrix[x][y]++;
-	
-	button.textContent = matrix[x][y];
 
-console.log(matrix[x][y]);	
-				
-				}
+}
+function getValues(button) {
+    let x_int = parseInt(button.value[0]);
+    let y_int = parseInt(button.value[1]);
+    let values = [(x_int - 3).toString() + y_int.toString()];
+    values.push(x_int.toString() + (y_int - 3).toString());
+    values.push((x_int - 2).toString() + (y_int - 2).toString());
+    values.push((x_int + 2).toString() + (y_int - 2).toString());
+    values.push((x_int + 3).toString() + y_int.toString());
+    values.push(x_int.toString() + (y_int + 3).toString());
+    values.push((x_int + 2).toString() + (y_int + 2).toString());
+    values.push((x_int - 2).toString() + (y_int + 2).toString());
+
+    return values;
+}
+
+function disableButtons(button) {
+   
+    let buttons = document.getElementsByClassName("buttons");
+
+    for (item of buttons) {
+		
+        if (!getValues(button).includes(item.value)) {
+            item.disabled = true;
+        } else if(!clickedButtons.includes(item.value))  {
+            item.disabled = false;
+			
+        }
+
+    }
+}
+
+function bestScore(button){
+
+}
+
 
 
 let myArray = createTable();
